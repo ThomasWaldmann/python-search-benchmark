166c1ef814eb71461682a1e7f9807f59a2da38bc
TW <tw@waldmann-edv.de>
2012-01-13 22:23:58 +0100

    move docs to README.txt

--- bench.py
+++ bench.py
@@ -3,32 +3,6 @@
 # License: MIT licensed
 """
 A simple python search library benchmark
-========================================
-
-Supporting:
- * whoosh (a search library in pure Python)
- * xappy / xapian (Python + C++)
-
-How it benchmarks
------------------
-First, we create a list of documents that are used for all the benchmarks
-(it uses exactly the same list of the same documents for all, of course).
-
-The documents contain some random word, the word length, and some extra fields
-to pump up the size of the document (you can create quite large indexes that
-way).
-
-Then we create an index for these documents, all fields are indexed/stored.
-
-Then we search the index for all words in random order and read all stored
-fields for the search results.
-
-Notes
------
-For whoosh indexing, there is a USE_MULTIPROCESSING setting.
-When using True, keep in mind that it could be not quite fair (as other
-indexers maybe don't use multiple cores / processes). But OTOH, if it
-is that easy, why not use it? :)
 """
 
 import codecs
